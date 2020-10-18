8073fb433b80db458e90a755359f77e0faeba181
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-10-18 16:54:52 +0200

    XML feldolgozása

--- MNBWebszolg/MNBWebszolg/Form1.cs
+++ MNBWebszolg/MNBWebszolg/Form1.cs
@@ -9,17 +9,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MNBWebszolg
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates;
+        string result;
         public Form1()
         {
             InitializeComponent();
             CallWebService();
             dataGridView1.DataSource = Rates;
+            XMLProcess();
         }
         private void CallWebService()
         {
@@ -31,7 +34,28 @@ namespace MNBWebszolg
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
+        }
+        private void XMLProcess()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit!=0)
+                {
+                    rate.Value = value / unit;
+                }
+                
+            }
+
         }
     }
 }
