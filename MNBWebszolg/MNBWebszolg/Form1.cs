1230e8640e9a376a742e24c46a51000416b1ad9c
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-10-18 17:50:50 +0200

    Dinakimus funkciók

--- MNBWebszolg/MNBWebszolg/Form1.cs
+++ MNBWebszolg/MNBWebszolg/Form1.cs
@@ -21,20 +21,24 @@ namespace MNBWebszolg
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+        }
+        private void RefreshData()
+        {
+            Rates.Clear();
             CallWebService();
             dataGridView1.DataSource = Rates;
             XMLProcess();
             CreateChart();
-           
         }
         private void CallWebService()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString(),
             };
             var response = mnbService.GetExchangeRates(request);
             result = response.GetExchangeRatesResult;
@@ -72,6 +76,18 @@ namespace MNBWebszolg
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
