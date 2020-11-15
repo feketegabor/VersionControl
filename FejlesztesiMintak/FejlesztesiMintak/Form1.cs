1b0ff00818c0716cd0d38b779cdf23af97472254
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-11-15 17:22:11 +0100

    Termékválasztó gombok

--- FejlesztesiMintak/FejlesztesiMintak/Form1.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Form1.cs
@@ -16,17 +16,23 @@ namespace FejlesztesiMintak
     {
         List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            { 
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
-            Factory = new CarFactory();
+            Factory = new BallFactory();
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
@@ -53,6 +59,26 @@ namespace FejlesztesiMintak
                 mainPanel.Controls.Remove(oldestToy);
                 _toys.Remove(oldestToy);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
