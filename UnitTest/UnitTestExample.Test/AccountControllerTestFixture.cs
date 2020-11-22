5f9618c3e99f8e58a24153419d362746c75a7589
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-11-22 16:32:55 +0100

    E-mail validálás teszt függvény

---
+++ UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFixture
+    {
+        [Test]
+        void TestValidateEmail(string email, bool expextedResult)
+        {
+
+        }
+    }
+}
