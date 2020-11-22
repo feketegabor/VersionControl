1df7d45fc52c331be2acf921917983c590786b3b
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-11-22 17:18:29 +0100

    E-mail validálás teszt kifejtése

---
+++ UnitTest/UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestExample;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnitTestExample.Abstractions;
+using UnitTestExample.Entities;
+using UnitTestExample.Services;
+
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        void TestValidateEmail(string email, bool expextedResult)
+        {
+            // Arrange
+            var accountController = new UnitTestExample.AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+    }
+}
