7c47f37c7b85328b34466557da2427c866e5fa90
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-11-29 13:04:25 +0100

    Játék kipróbálása

--- EvoluciosGyakorlat/EvoluciosGyakorlat/Form1.cs
+++ EvoluciosGyakorlat/EvoluciosGyakorlat/Form1.cs
@@ -21,6 +21,8 @@ namespace EvoluciosGyakorlat
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            //gc.AddPlayer();
+            //gc.Start(true);
         }
     }
 }
