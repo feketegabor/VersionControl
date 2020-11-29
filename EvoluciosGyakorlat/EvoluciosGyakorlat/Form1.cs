25e19286fa5be856fe17c048e5ab4c6e651996da
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2020-11-29 13:35:59 +0100

    Új generáció

--- EvoluciosGyakorlat/EvoluciosGyakorlat/Form1.cs
+++ EvoluciosGyakorlat/EvoluciosGyakorlat/Form1.cs
@@ -43,13 +43,28 @@ namespace EvoluciosGyakorlat
         private void Gc_GameOver(object sender)
         {
             generation++;
-            label1.Text = string.Format(
-                "{0}. generáció",
-                generation);
+            label1.Text = string.Format("{0}. generáció", generation);
+
             var playerList = from p in gc.GetCurrentPlayers()
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
